9479b014764d5eaa9f298b13030bee1301ccff4e
Rion Hutsell <st.gish@gmail.com>
2020-08-22 19:26:54 -0600

    Add self.update_or_create method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  def self.update_or_create(auth)
+    user = User.find_or_create_by(uid: auth[:uid]) do |user|
+      user.username = auth.info.email
+      user.uid = auth.uid
+    end
+  end
 end
