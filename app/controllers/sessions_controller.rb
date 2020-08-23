0e85d3eab1b67eeb7681451c8c03aea297d747f0
Rion Hutsell <st.gish@gmail.com>
2020-08-22 19:25:54 -0600

    Add sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def create
+    auth = request.env['omniauth.auth']
+    user = User.update_or_create(auth)
+    user.google_token = auth.credentials.token
+
+    refresh_token = auth.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+
+    user.save
+
+    session[:user_id] = user.id
+    redirect_to '/dashboard'
   end
 end
