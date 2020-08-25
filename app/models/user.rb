6cef41ef93b4b20a51f9e85e0daf6c2baff8e894
Cam Cielo <ckc.cameron@gmail.com>
2020-08-24 18:40:16 -0600

    Add has_many relationships to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   validates :uid, uniqueness: true, presence: true
   validates :google_token, uniqueness: true
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   def self.update_or_create(auth)
     user = User.find_or_create_by(uid: auth[:uid]) do |user|
       user.username = auth[:info][:email]
