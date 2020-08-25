d29fc2f06fde2a003ab01bd788c2f6e854347a2e
Cam Cielo <ckc.cameron@gmail.com>
2020-08-25 13:13:51 -0600

    Create MovieService file with API call methods

---
+++ app/services/movie_service.rb
@@ -0,0 +1,24 @@
+class MovieService
+  def conn
+    Faraday.new('https://api.themoviedb.org/3/') do |f|
+      f.params[:api_key] = ENV['movie_db_api_key']
+    end
+  end
+
+  def json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def search_results(query)
+    json("search/movie?query=#{query}")
+  end
+
+  def db_top_rated_movies
+    json("movie/top_rated?")
+  end
+
+  def movie_data(movie_id)
+    json("movie/#{movie_id}")
+  end
+end
