25681bc7887eaf246398b0f6e18af2be6420dd8a
Cam Cielo <ckc.cameron@gmail.com>
2020-08-24 18:04:57 -0600

    Add generate_flash method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def generate_flash(resource)
+    resource.errors.messages.each do |validation, message|
+      flash[validation] = "#{validation}: #{message}"
+    end
+  end
 end
