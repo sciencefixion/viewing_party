13fb8b375b8d4688a420fefc239f029ff17629ec
Cam Cielo <ckc.cameron@gmail.com>
2020-08-25 12:57:04 -0600

    Create SearchResults PORO for top rated movies and search result movies

---
+++ app/poros/search_results.rb
@@ -0,0 +1,26 @@
+class SearchResults
+  def top_rated_movies
+    json = service.db_top_rated_movies
+    movies = json[:results].map do |movie_data|
+      movie_result(movie_data)
+    end
+    movies.first(40)
+  end
+
+  def movie_search_results(search_query)
+    json = service.search_results(search_query)
+    movies = json[:results].map do |movie_data|
+      movie_result(movie_data)
+    end
+    movies.first(40)
+  end
+
+  private
+  def service
+    MovieService.new
+  end
+
+  def movie_result(data)
+    MovieResult.new(data)
+  end
+end
