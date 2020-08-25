b1ef3eb23f78a47f994a8f7ac0a49f26078fc0c3
Cam Cielo <ckc.cameron@gmail.com>
2020-08-24 18:48:41 -0600

    Add methods to Friendship model to recognize friendships between users

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,17 @@ class Friendship
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   validates_uniqueness_of :friend, scope: :user, message: "You have already added this friend"
+
+  def self.create_reciprocal_for_ids(user_id, friend_id)
+    user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
+    friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
+    [user_friendship, friend_friendship]
+  end
+
+  def self.destroy_reciprocal_for_ids(user_id, friend_id)
+    friendship1 = Friendship.find_by(user_id: user_id, friend_id: friend_id)
+    friendship2 = Friendship.find_by(user_id: friend_id, friend_id: user_id)
+    friendship1.destroy
+    friendship2.destroy
+  end
 end
