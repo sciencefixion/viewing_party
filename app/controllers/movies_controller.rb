b8455cf77005a46ab0dbf88ba8f1c91e918f4a0f
Cam Cielo <ckc.cameron@gmail.com>
2020-08-26 15:09:46 -0600

    Remove commented out code before MoviesController refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,13 +7,6 @@ class MoviesController < ApplicationController
       @movies = SearchResults.new.top_rated_movies
     end
   end
-  #   search_results = SearchResults.new
-  #   if params[:search]
-  #     @movies = search_results.movie_search_results(params[:search])
-  #   else
-  #     @movies = search_results.top_rated_movies
-  #   end
-  # end
 
   def show
     movie = SearchResults.new
