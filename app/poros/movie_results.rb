e91729479cfaed84354a9346b4c43d1d6d28b8d9
Cam Cielo <ckc.cameron@gmail.com>
2020-08-25 21:21:53 -0600

    Add formatting helper methods to MovieResults

--- app/poros/movie_results.rb
+++ app/poros/movie_results.rb
@@ -12,7 +12,33 @@ class MovieResults
     @vote_average = data[:vote_average]
     @runtime = data[:runtime]
     @genres = data[:genres]
-    @summary = data[:title]
+    @summary = data[:overview]
+  end
+
+  def format_runtime
+    "#{@runtime/60} hr #{@runtime % 60} min"
+  end
+
+  def ten_cast_members
+    cast = search.cast(@id)
+    ten_cast_members = cast[:cast].first(10)
+  end
+
+  def reviews
+    reviews = search.reviews(@id)
+    reviews[:results]
+  end
+
+  def count_of_reviews
+    reviews.size
+  end
+
+  def genres_names
+    genre_names = []
+    @genres.each do |genre|
+      genre_names << genre[:name]
+    end
+    genre_names.join(", ")
   end
 
   private
