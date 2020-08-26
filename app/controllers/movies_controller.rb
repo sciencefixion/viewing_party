ced7b13c23927f05280092fd80bcce8e9d4240bb
Cam Cielo <ckc.cameron@gmail.com>
2020-08-26 15:09:08 -0600

    Refactor MoviesController index method to use SearchResults within if/else block

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,19 @@
 class MoviesController < ApplicationController
   def index
-    search_results = SearchResults.new
-    if params[:search]
-      @movies = search_results.movie_search_results(params[:search])
+    search = params[:search]
+    if search
+      @movies = SearchResults.new.movie_search_results(search)
     else
-      @movies = search_results.top_rated_movies
+      @movies = SearchResults.new.top_rated_movies
     end
   end
+  #   search_results = SearchResults.new
+  #   if params[:search]
+  #     @movies = search_results.movie_search_results(params[:search])
+  #   else
+  #     @movies = search_results.top_rated_movies
+  #   end
+  # end
 
   def show
     movie = SearchResults.new
