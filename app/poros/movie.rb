e48436c6f2ef60b10c3e7ca9786da71057f0a12b
Cam Cielo <ckc.cameron@gmail.com>
2020-08-27 07:28:52 -0600

    Refactor MovieResults into Movie PORO

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,49 @@
 class Movie
-  attr_reader :title,
-              :vote_average
+  attr_reader :id,
+              :title,
+              :vote_average,
+              :runtime,
+              :genres,
+              :summary
 
-  def initialize(movie_info)
-    @title = movie_info[:title]
-    @vote_average = movie_info[:vote_average]
+  def initialize(data)
+    @id = data[:id]
+    @title = data[:title]
+    @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:overview]
+  end
+
+  def format_runtime
+    "#{@runtime / 60} hr #{@runtime % 60} min"
+  end
+
+  def ten_cast_members
+    cast = search.cast(@id)
+    cast[:cast].first(10)
+  end
+
+  def reviews
+    reviews = search.reviews(@id)
+    reviews[:results]
+  end
+
+  def count_of_reviews
+    reviews.size
+  end
+
+  def genres_names
+    genre_names = []
+    @genres.each do |genre|
+      genre_names << genre[:name]
+    end
+    genre_names.join(', ')
+  end
+
+  private
+
+  def search
+    SearchResults.new
   end
 end
