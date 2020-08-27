2dd5f16d6920599e63b85a2734cc50507636719b
Cam Cielo <ckc.cameron@gmail.com>
2020-08-27 08:35:13 -0600

    Remove to do text

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -12,15 +12,6 @@ RSpec.describe "New viewing party page" do
   end
 
   it "displays a form to create a new viewing party" do
-    #     As an authenticated user,
-    # When I visit the new viewing party page,
-    # I should see a form with the following:
-    #
-    #   Movie Title (that is un-editable)
-    #   Duration of Party with a default value of movie runtime in minutes
-    #   When: as a date picker
-    #   Checkboxes next to each friend (if user has friends)
-    #   Button to create party
     search = SearchResults.new
     movie = search.details(@movie_id)
     friend1 = create(:user, username: 'test1@test.com')
@@ -31,8 +22,6 @@ RSpec.describe "New viewing party page" do
     Friendship.create_reciprocal_for_ids(@user.id, friend3.id)
 
     visit '/viewing-party/new'
-    binding.pry
-
 
     expect(page).to have_content(movie.title)
     expect(page).to have_content(movie.runtime)
