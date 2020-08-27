7e008aac92e2b3d3b97c75f8c0cebbd16ac410f3
Cam Cielo <ckc.cameron@gmail.com>
2020-08-27 09:04:02 -0600

    Remove code brainstorming

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,5 @@
 class PartiesController < ApplicationController
   def new
-    # movie_data = SearchResults.new.details(movie_id)
     @movie = Movie.new(movie_title: params[:title], duration: params[:runtime])
-    # binding.pry
   end
 end
